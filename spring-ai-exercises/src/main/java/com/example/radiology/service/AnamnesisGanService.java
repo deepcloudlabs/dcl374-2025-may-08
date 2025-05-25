a5138148b8da7ecde6d5fc05bac74ef6d25be87d
Binnur KURT <deepcloudlabs@gmail.com>
2025-05-25 23:50:49 +0300

    Update AnamnesisGanService.java

--- spring-ai-exercises/src/main/java/com/example/radiology/service/AnamnesisGanService.java
+++ spring-ai-exercises/src/main/java/com/example/radiology/service/AnamnesisGanService.java
@@ -3,8 +3,6 @@ package com.example.radiology.service;
 import com.example.radiology.dto.request.GenerateAnamnesisRequest;
 import com.example.radiology.dto.response.GenerateAnamnesisResponse;
 
-import reactor.core.publisher.Mono;
-
 public interface AnamnesisGanService {
 	GenerateAnamnesisResponse generateAnemnesis(GenerateAnamnesisRequest request);
 }	
